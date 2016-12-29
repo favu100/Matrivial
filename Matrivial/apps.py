e60df67e6482029bd89274ea22c8df570cfb0663
favu100 <Fabian.Vu@hhu.de>
2016-12-29 20:53:46 +0100

    initial commit

---
+++ Matrivial/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MatrivialConfig(AppConfig):
+    name = 'Matrivial'
---
+++ Matrivial/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+import numpy as np;
+from sympy import *
+
+def index(request):
+   A = Matrix([[1,2],[3,4]])
+   return HttpResponse(A @ A)
